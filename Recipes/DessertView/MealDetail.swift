a74b0853670f746d54664efde03533297661f361
CJ <jayyang116@gmail.com>
2024-03-09 23:37:42 -0500

    add a transition view

--- Recipes/DessertView/MealDetail.swift
+++ Recipes/DessertView/MealDetail.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct MealDetail: View {
     @Environment(MealsManager.self) private var manager
     @State private var themes: [Color] = [Color.green, Color.orange, Color.blue, Color.pink, Color.yellow, Color.purple]
-    @State private var mealDetail: Meal.Detail?
+    @State private var mealDetail: (Meal.Detail?, Error?)
     @State private var image: UIImage?
     @State private var meal: Meals.Meal
     
@@ -19,48 +19,43 @@ struct MealDetail: View {
     }
 
     var body: some View {
-        detailView
-            .ignoresSafeArea(edges: .top)
-            .task {
-                mealDetail = await manager.fetchMealDetail(meal)
-                image = await manager.getImage(from: meal.strMealThumb)
+        VStack(spacing: Constant.MealDetail.outermostVStackSpacing) {
+            dessertImage
+            ZStack {
+                RoundedRectangle(cornerRadius: Constant.MealDetail.cornerRadius)
+                    .fill(.white.opacity(Constant.MealDetail.roundedRecOpacity))
+                detailContent
             }
+            .scrollIndicators(.hidden)
+        }
+        .ignoresSafeArea(edges: .top)
+        .task {
+            mealDetail = await manager.fetchMealDetail(meal)
+            image = await manager.getImage(from: meal.strMealThumb)
+        }
     }
     
     @ViewBuilder
-    private var detailView: some View {
-        if mealDetail != nil {
-            VStack(spacing: Constant.MealDetail.outermostVStackSpacing) {
-                dessertImage
-                ZStack {
-                    RoundedRectangle(cornerRadius: Constant.MealDetail.cornerRadius)
-                        .fill(.white.opacity(Constant.MealDetail.roundedRecOpacity))
-                    ScrollView {
-                        VStack(spacing: Constant.MealDetail.mainContentVStackSpacing) {
-                            headingSection
-                            ingredientsSection
-                            instructionsSection
-                        }
-                    }
-                    .padding(.bottom)
-                    .clipShape(RoundedRectangle(cornerRadius: Constant.MealDetail.cornerRadius))
+    private var detailContent: some View {
+        if mealDetail.0 != nil {
+            ScrollView {
+                VStack(spacing: Constant.MealDetail.mainContentVStackSpacing) {
+                    headingSection
+                    ingredientsSection
+                    instructionsSection
                 }
             }
-            .scrollIndicators(.hidden)
+            .padding(.bottom)
+            .clipShape(RoundedRectangle(cornerRadius: Constant.MealDetail.cornerRadius))
+        } else if mealDetail.1 == nil {
+            ProgressView()
         } else {
-            VStack(spacing: Constant.MealDetail.outermostVStackSpacing) {
-                dessertImage
-                ZStack {
-                    RoundedRectangle(cornerRadius: Constant.MealDetail.cornerRadius)
-                        .fill(.white.opacity(Constant.MealDetail.roundedRecOpacity))
-                    Text("Instructions will be added soon.")
-                }
-            }
+            Text("Instructions will be added soon.")
         }
     }
     
     private var headingSection: some View {
-        Text(mealDetail?.strMeal ?? "")
+        Text(mealDetail.0?.strMeal ?? "")
             .font(.system(size: Constant.MealDetail.headingSecFontSize, weight: .semibold))
             .padding(.top, Constant.MealDetail.headingSecTopPadding)
     }
@@ -71,15 +66,15 @@ struct MealDetail: View {
                 Text("Ingredients")
                     .font(.system(size: Constant.MealDetail.subtitleFontSize, weight: .medium))
                 Spacer()
-                Text("\(mealDetail?.strIngredients.count ?? 0) Item(s)")
+                Text("\(mealDetail.0?.strIngredients.count ?? 0) Item(s)")
                     .foregroundStyle(.gray)
             }
             .padding(.horizontal)
 
             ScrollView(.horizontal) {
                 HStack {
-                    ForEach(0..<(mealDetail?.strIngredients.count ?? 0), id: \.self) { index in
-                        if let ingredient = mealDetail?.strIngredients[index], let measure = mealDetail?.strMeasures[index] {
+                    ForEach(0..<(mealDetail.0?.strIngredients.count ?? 0), id: \.self) { index in
+                        if let ingredient = mealDetail.0?.strIngredients[index], let measure = mealDetail.0?.strMeasures[index] {
                             IngredientCard(ingredient: ingredient, measure: measure, theme: themes[index % themes.count])
                         }
                     }
@@ -93,7 +88,7 @@ struct MealDetail: View {
         VStack(alignment: .leading, spacing: Constant.MealDetail.instructionsSecSpacing) {
             Text("Instructions")
                 .font(.system(size: Constant.MealDetail.subtitleFontSize, weight: .medium))
-            Text(mealDetail?.strInstructions ?? "")
+            Text(mealDetail.0?.strInstructions ?? "")
         }
         .padding(.horizontal)
     }
