4a508f49bd3a4b2bebbada4be2c753eaed980d9d
CJ <128767615+CJRookie@users.noreply.github.com>
2024-03-21 01:27:34 -0400

    mock a url session

---
+++ RecipesTests/MockURLProtocol.swift
@@ -0,0 +1,40 @@
+//
+//  MockURLProtocol.swift
+//  RecipesTests
+//
+//  Created by CJ on 3/19/24.
+//
+
+import XCTest
+@testable import Recipes
+
+class MockURLProtocol: URLProtocol {
+    static var requestHandler: ((URLRequest) throws -> (HTTPURLResponse, Data))?
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override func startLoading() {
+        
+        guard let handler = MockURLProtocol.requestHandler else {
+            XCTFail("No request handler provided.")
+            return
+        }
+        
+        do {
+            let (response, data) = try handler(request)
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            client?.urlProtocol(self, didLoad: data)
+            client?.urlProtocolDidFinishLoading(self)
+        } catch {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+    }
+    
+    override func stopLoading() {}
+}
