d0eaed59b3be545ff309d900202a18669a838965
CJ <128767615+CJRookie@users.noreply.github.com>
2024-03-25 00:10:43 -0400

    change var names

--- Recipes/Models/MealDetails.swift
+++ Recipes/Models/MealDetails.swift
@@ -56,7 +56,7 @@ struct Detail: Codable, Identifiable, Hashable {
     let measure19: String
     let measure20: String
     
-    var strIngredients: [String] {
+    var ingredients: [String] {
         let ingredients = [ingredient1, ingredient2, ingredient3, ingredient4, ingredient5,
                            ingredient6, ingredient7, ingredient8, ingredient9, ingredient10,
                            ingredient11, ingredient12, ingredient13, ingredient14, ingredient15,
@@ -65,7 +65,7 @@ struct Detail: Codable, Identifiable, Hashable {
         return ingredients.filter { !$0.isEmpty }
     }
 
-    var strMeasures: [String] {
+    var measures: [String] {
         let measures = [measure1, measure2, measure3, measure4, measure5,
                         measure6, measure7, measure8, measure9, measure10,
                         measure11, measure12, measure13, measure14, measure15,
