ba3d5563de0c0f50afacb24d20d38b3a0e0d0c7e
CJ <128767615+CJRookie@users.noreply.github.com>
2024-03-25 00:14:14 -0400

    change to singleton

--- Recipes/Utilities/ImageCacheCenter.swift
+++ Recipes/Utilities/ImageCacheCenter.swift
@@ -10,14 +10,12 @@ import UIKit
 
 @Observable
 class ImageCacheCenter {
-    private let sharedURLCache: URLCache
-    private let networkDataRetriever: NetworkDataService
+    static let shared = ImageCacheCenter()
+    private let sharedURLCache: URLCache = .shared
+    private let networkDataRetriever: NetworkDataService = RecipeDataRetriever()
     private let maxMemoryCacheSize: Int = 100 * 1024 * 1024
-    var error: Error?
     
-    init(sharedURLCache: URLCache = .shared, networkDataRetriever: NetworkDataService = RecipeDataRetriever()) {
-        self.sharedURLCache = sharedURLCache
-        self.networkDataRetriever = networkDataRetriever
+    private init() {
         sharedURLCache.diskCapacity = 0
         sharedURLCache.memoryCapacity = maxMemoryCacheSize
     }
@@ -25,15 +23,15 @@ class ImageCacheCenter {
     /// Retrieves a meal image from the specified URL.
     /// - Parameter url: The URL from which to retrieve/fetch the meal image.
     /// - Returns: A `UIImage` representing the fetched meal image, or `nil` if the image is not available.
-    func getRecipeImage(from url: URL) async -> UIImage? {
+    func getImage(from url: String) async -> UIImage? {
+        guard let url = URL(string: url) else { return nil }
         let request = URLRequest(url: url)
         if let cachedData = sharedURLCache.cachedResponse(for: request)?.data, let image = UIImage(data: cachedData) {
             return image
         } else {
             do {
-                return try await fetchRecipeImage(from: url)
+                return try await fetchImage(from: url)
             } catch {
-                self.error = error
                 return nil
             }
         }
@@ -42,10 +40,10 @@ class ImageCacheCenter {
     /// Fetches a meal image from the specified URL.
     /// - Parameter url: The URL from which to fetch the meal image.
     /// - Returns: A `UIImage` representing the fetched meal image, or `nil` if the image is not available.
-    private func fetchRecipeImage(from url: URL) async throws -> UIImage? {
+    private func fetchImage(from url: URL) async throws -> UIImage? {
         let downloadedData = try await networkDataRetriever.downloadData(from: url)
         let image = UIImage(data: downloadedData.0)
-        cacheRecipeImage(from: url, response: downloadedData.1, data: downloadedData.0)
+        cacheImage(from: url, response: downloadedData.1, data: downloadedData.0)
         return image
     }
     
@@ -54,7 +52,7 @@ class ImageCacheCenter {
     ///   - url: The URL from which the meal image was fetched.
     ///   - response: The URLResponse received during the image fetching process.
     ///   - data: The data representing the fetched meal image.
-    private func cacheRecipeImage(from url: URL, response: URLResponse, data: Data) {
+    private func cacheImage(from url: URL, response: URLResponse, data: Data) {
         let request = URLRequest(url: url)
         let cachedURLResponse = CachedURLResponse(response: response, data: data, storagePolicy: .allowedInMemoryOnly)
         sharedURLCache.storeCachedResponse(cachedURLResponse, for: request)
