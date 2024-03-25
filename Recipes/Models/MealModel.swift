4108a62f155123eaadee5934c827c40526b89a2c
CJ <128767615+CJRookie@users.noreply.github.com>
2024-03-25 00:44:37 -0400

    update constants

---
+++ Recipes/Models/MealModel.swift
@@ -0,0 +1,8 @@
+//
+//  MealModel.swift
+//  Recipes
+//
+//  Created by CJ on 3/25/24.
+//
+
+import Foundation
--- Recipes/Utilities/Constant.swift
+++ Recipes/Utilities/Constant.swift
@@ -15,34 +15,30 @@ struct Constant {
         static let overlayPadding: CGFloat = 8
     }
     
-    struct IngredientCard {
-        static let size: CGFloat = 100
+    struct IngredientRow {
+        static let imagePadding: CGFloat = 4
+        static let rowHeight: CGFloat = 48
         static let cornerRadius: CGFloat = 8
         static let themeOpacity: CGFloat = 0.1
-        static let measureVPadding: CGFloat = 2
-        static let textLineLimit: Int = 3
-        static let trailingPadding: CGFloat = 4
     }
     
     struct MealDetail {
         static let outermostVStackSpacing: CGFloat = 32
         static let cornerRadius: CGFloat = 40
         static let roundedRecOpacity: CGFloat = 0.8
-        static let mainContentVStackSpacing: CGFloat = 36
+        static let mainContentVStackSpacing: CGFloat = 24
         static let headingSecFontSize: CGFloat = 28
         static let headingSecTopPadding: CGFloat = 24
-        static let instructionsSecSpacing: CGFloat = 16
-        static let subtitleFontSize: CGFloat = 18
         static let imageHeight: CGFloat = 160
         static let favoriteButtonYOffset: CGFloat = -290
         static let favoriteButtonBGPadding: CGFloat = 4
         static let favoriteButtonSize: CGFloat = 40
     }
     
-    struct CategoryBar {
+    struct NavigationBar {
         static let roundedRecCornerRadius: CGFloat = 8
         static let roundedRecFillColorOpacity: CGFloat = 0.1
-        static let frameWidth: CGFloat = 88
+        static let frameWidth: CGFloat = 108
         static let frameHeight: CGFloat = 36
         static let animationDuration: CGFloat = 0.5
         static let vPadding: CGFloat = 4
@@ -53,5 +49,6 @@ struct Constant {
         static let categoriesURLKey = "Categories_URL"
         static let categoryBaseURLKey = "Category_Base_URL"
         static let detailBaseURLkey = "Detail_Base_URL"
+        static let ingredientImageBaseURLKey = "Ingredient_Image_Base_URL"
     }
 }
