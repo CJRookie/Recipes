715ae14ddc5f8758f374981183caae81a969a282
CJ <128767615+CJRookie@users.noreply.github.com>
2024-03-26 11:38:05 -0400

    add a reusable list template view

--- Recipes/TabViews/Components/ListTemplate.swift
+++ Recipes/TabViews/Components/ListTemplate.swift
@@ -8,7 +8,31 @@
 import SwiftUI
 
 struct ListTemplate: View {
+    @State private var text: String = ""
+    let list: [Meal]
+    let navTitle: String
+    
+    var currentList: [Meal] {
+        MealListOperation.filter(list, by: text)
+    }
+    
     var body: some View {
-        
+        List(currentList) { meal in
+            ZStack {
+                MealRow(meal: meal)
+                NavigationLink(value: meal) {
+                    EmptyView()
+                }
+                .opacity(0)
+            }
+            .listRowSeparator(.hidden)
+        }
+        .listStyle(.plain)
+        .navigationTitle(navTitle)
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationDestination(for: Meal.self) { meal in
+            MealDetail(meal: meal)
+        }
+        .searchable(text: $text, placement: .navigationBarDrawer(displayMode: .always), prompt: "Search by name")
     }
 }
