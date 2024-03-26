50b7ca91b774b0444470a919b36a45f52f56afab
CJ <128767615+CJRookie@users.noreply.github.com>
2024-03-26 11:42:56 -0400

    refactor code snippet with the reusable list template view

--- Recipes/TabViews/CategoryTab/MealList.swift
+++ Recipes/TabViews/CategoryTab/MealList.swift
@@ -8,35 +8,13 @@
 import SwiftUI
 
 struct MealList: View {
-    @Environment(FavoriteListManager.self) private var favoriteListManager
     @State private var manager = MealListManager()
-    @State private var text: String = ""
     let category: String
     
-    var currentList: [Meal] {
-        MealListOperation.filter(manager.mealList, by: text)
-    }
-    
     var body: some View {
-        List(currentList) { meal in
-            ZStack {
-                MealRow(meal: meal)
-                NavigationLink(value: meal) {
-                    EmptyView()
-                }
-                .opacity(0)
+        ListTemplate(list: manager.mealList, navTitle: category)
+            .task {
+                await manager.fetchMeals(in: category)
             }
-            .listRowSeparator(.hidden)
-        }
-        .listStyle(.plain)
-        .navigationTitle(category)
-        .navigationBarTitleDisplayMode(.inline)
-        .navigationDestination(for: Meal.self) { meal in
-            MealDetail(meal: meal, favorites: favoriteListManager.favoriteRecipes)
-        }
-        .searchable(text: $text, placement: .navigationBarDrawer(displayMode: .always), prompt: "Search by name")
-        .task {
-            await manager.fetchMeals(in: category)
-        }
     }
 }
