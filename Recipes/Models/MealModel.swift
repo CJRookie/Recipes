ad39b6d631464f4073a8067f8eac0f2ea6bfcbca
CJ <128767615+CJRookie@users.noreply.github.com>
2024-03-26 11:33:22 -0400

    move the source file to another group

--- Recipes/Models/MealModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  MealModel.swift
-//  Recipes
-//
-//  Created by CJ on 3/25/24.
-//
-
-import Foundation
---
+++ Recipes/TabViews/Components/ListTemplate.swift
@@ -0,0 +1,14 @@
+//
+//  ListTemplate.swift
+//  Recipes
+//
+//  Created by CJ on 3/26/24.
+//
+
+import SwiftUI
+
+struct ListTemplate: View {
+    var body: some View {
+        
+    }
+}
