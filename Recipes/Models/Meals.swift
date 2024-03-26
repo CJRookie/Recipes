1ed2e1cd3aa2ec32c630d0aa6fcba1f3f86476e4
CJ <128767615+CJRookie@users.noreply.github.com>
2024-03-26 11:46:21 -0400

    Update codable struct to codable SwiftData model

--- Recipes/Models/Meals.swift
+++ Recipes/Models/Meals.swift
@@ -6,19 +6,47 @@
 //
 
 import Foundation
+import SwiftData
 
 struct Meals: Codable {
     let meals: [Meal]
 }
 
-struct Meal: Codable, Identifiable, Hashable {
-    let name: String
-    let thumb: String
-    let id: String
+@Model
+class Meal: Codable, Identifiable, Hashable {
+    var name: String
+    var thumb: String
+    @Attribute(.unique) var id: String
+    var isFavorite: Bool = false
+    var dateOfCreation: Date = Date.now
+    var isAdded: Bool = false
     
     enum CodingKeys: String, CodingKey {
         case name = "strMeal"
         case thumb = "strMealThumb"
         case id = "idMeal"
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.thumb = try container.decode(String.self, forKey: .thumb)
+        self.id = try container.decode(String.self, forKey: .id)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+        try container.encode(thumb, forKey: .thumb)
+        try container.encode(id, forKey: .id)
+    }
+    
+    init(name: String, thumb: String, id: String, isFavorite: Bool = false, dateOfCreation: Date = .now, isAdded: Bool = false) {
+        self.name = name
+        self.thumb = thumb
+        self.id = id
+        self.isFavorite = isFavorite
+        self.dateOfCreation = dateOfCreation
+        self.isAdded = isAdded
     }
 }
